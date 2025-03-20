f3c958bd1f10416dfa89a0cce14e9ac5e0e2f791
SivaNarayana <e.siva306@gmail.com>
2025-03-20 16:15:48 +0530

    Just Started

---
+++ src/main/java/com/demo/BasicAPICreation/BasicApiCreationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.BasicAPICreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApiCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApiCreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/BasicAPICreation/BasicApiCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.BasicAPICreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
